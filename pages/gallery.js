d1fefdedd417cf7807798a0c2005dea2b37c01f4
Catu <catu@Catus-MBP.fios-router.home>
2021-06-09 12:40:37 -0400

    set up gallery :))))

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,33 +1,41 @@
-
 import React from "react";
+import Masonry from "react-masonry-css";
 import { getGallery } from "./api/case-study";
+import { RichText } from "prismic-reactjs";
 import Navigation from "../components/nav";
 import Footer from "../components/footer";
 import "animate.css";
 
-const Gallery = ({body}) => {
-    return (
+const Gallery = ({ body }) => {
+  return (
     <>
       <Navigation />
-      <div>
-          {/* {body.map((galleryItem, i) => (
-            //   console.log(galleryItem)
-              <img src={galleryItem.image.url} />
-            ))
-          } */}
+      <div className="gallery-container">
+      <Masonry data-scroll-section
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {body.map((galleryItem, i) => (
+          <div data-scroll className="gallery-item" key={i}>
+            <img src={galleryItem.image.url} />
+            <p>  <RichText render={galleryItem.image_caption} /></p>
+          </div>
+        ))}
+      </Masonry>
       </div>
-      <Footer/>
-      </>
-    )
-}
+      <Footer />
+    </>
+  );
+};
 
 export async function getStaticProps() {
-    const gallery = await getGallery();
-    return {
-      props: {
-        body: gallery.data.gallery_content,
-      },
-    };
-  }
-  
+  const gallery = await getGallery();
+  return {
+    props: {
+      body: gallery.data.gallery_content,
+    },
+  };
+}
+
 export default Gallery;
